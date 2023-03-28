d7f263aa65f212a9daec531bec6f8a7891d832b8
vovademar <v.medvedev@g.nsu.ru>
2023-03-28 17:18:16 +0700

    deserialize

--- src/main/java/nsu/Main.java
+++ src/main/java/nsu/Main.java
@@ -3,6 +3,8 @@ package nsu;
 import nsu.framework.Persistence;
 import nsu.testing.Citizen;
 import nsu.testing.Person;
+import nsu.testing.Student;
+import nsu.testing.Subjects;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
@@ -10,16 +12,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws Exception {
         Person person = new Person("John", 20);
-        String serialized = Persistence.persist(person).toString();
+        //String serialized = Persistence.persist(person).toString();
         //System.out.println(serialized);
         List<String> stringList = new ArrayList<>();
         stringList.add("first");
         stringList.add("second");
-        Citizen citizen = new Citizen("Novosibirsk", 1, person, stringList);
+        Citizen citizen = new Citizen("Novosibirsk", person, stringList);
         String serialized1 = Persistence.persist(citizen).toString();
         System.out.println(serialized1);
+
+        Citizen c = (Citizen) Persistence.deserializeObject(serialized1, null);
+        assert c != null;
+        String serializedDeserialize = Persistence.persist(c).toString();
+        System.out.println(serializedDeserialize);
+
+        ArrayList<String> days = new ArrayList<>();
+        days.add("monday");
+        days.add("friday");
+        Student ex = new Student("Alex", 21, new Subjects(2128,"Math", days));
+
+        //ex.setPrikol(new Subjects(1,"tmp"));
+        String res = Persistence.persist(ex).toString();
+        System.out.println(res);
+
+        Student p = (Student) Persistence.deserializeObject(res, null);
+        assert p != null;
+        System.out.println(p.getSubjects());
+        System.out.println(p.getAge());
+        System.out.println(p.getName());
+
+
 //        Persistence.flush(Persistence.persist(citizen));
 //        var objectFields = PersistenceFramework.getFields(person.getClass(),person);
 //        System.out.println(objectFields);
---
+++ src/main/java/nsu/testing/Subjects.java
@@ -0,0 +1,39 @@
+package nsu.testing;
+
+import nsu.framework.Serialize;
+
+import java.util.ArrayList;
+
+@Serialize(serializeAll = true)
+public class Subjects {
+    public int roomNumber = 0;
+    public String subjectName = "Some string";
+    public ArrayList<String> days = new ArrayList<>();
+
+    public ArrayList<String> getDays() {
+        return days;
+    }
+
+    public Subjects(){}
+
+    public Subjects( int roomNum, String subjectName){
+        this.roomNumber = roomNum;
+        this.subjectName = subjectName;
+    }
+
+    public Subjects(int i, String subjectName, ArrayList<String> collection){
+        this.roomNumber = i;
+        this.subjectName = subjectName;
+        this.days = collection;
+    }
+
+    @Override
+    public String toString() {
+        return "subjects:{" +
+                "i=" + roomNumber +
+                ", str='" + subjectName + '\'' +
+                ", coll=" + days +
+                '}';
+    }
+}
+
